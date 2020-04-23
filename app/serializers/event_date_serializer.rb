a10b67437e2a54b2006012a24654631b04acd29b
seanlawrenz <seanlaw27@gmail.com>
2020-04-23 18:48:30 -0400

    fix: Issue 76 update time format on start and end times (#2)

--- app/serializers/event_date_serializer.rb
+++ app/serializers/event_date_serializer.rb
@@ -11,13 +11,23 @@ class EventDateSerializer < ActiveModel::Serializer
   attribute :start_time do
     start_time = object.start_time_key.to_s
     start_time = "0#{start_time}" if start_time.length == 3
-    Time.strptime(start_time, '%H%M').strftime('%I:%M %p')
+    minutes = start_time.last(2)
+    if minutes == '00'
+      Time.strptime(start_time, '%H%M').strftime('%l %p').lstrip
+    else
+      Time.strptime(start_time, '%H%M').strftime('%l:%M %p').lstrip
+    end
   end
 
   attribute :end_time do
     end_time = object.end_time_key.to_s
     end_time = "0#{end_time}" if end_time.length == 3
-    Time.strptime(end_time, '%H%M').strftime('%I:%M %p')
+    minutes = end_time.last(2)
+    if minutes == '00'
+      Time.strptime(end_time, '%H%M').strftime('%l %p').lstrip
+    else
+      Time.strptime(end_time, '%H%M').strftime('%l:%M %p').lstrip
+    end
   end
 
   attribute :date do
