415be099dd7f4fe34296ea9479c04153ad728842
Mahesh Kurma <maheshruby405@gmail.com>
2020-06-09 17:34:57 -0400

    more spec coverage

--- app/controllers/api/event_hours_controller.rb
+++ app/controllers/api/event_hours_controller.rb
@@ -4,6 +4,7 @@ module Api
   # Controller to expose Event Hours
   class EventHoursController < ApplicationController
     before_action :set_event_date, only: %i[show index]
+    before_action :set_event_hour, only: %i[show]
 
     # GET api/event_hours
     # GET api/event_dates/:id/event_hours
@@ -22,13 +23,19 @@ module Api
     # GET /api/event_hours/:id
     def show
       render json:
-        ActiveModelSerializers::SerializableResource.new(@event_date).as_json
+        ActiveModelSerializers::SerializableResource.new(@event_hour).as_json
     end
 
     private
 
     def set_event_date
+      return unless params[:event_date_id]
+
       @event_date = EventDate.find(params[:event_date_id])
+    end
+
+    def set_event_hour
+      @event_hour = EventHour.find(params[:id] || params[:event_hour_id])
     end
   end
 end
