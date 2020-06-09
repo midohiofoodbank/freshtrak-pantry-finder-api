55e90a61f4deaeba5fc439444a258e54be72772b
Mahesh Kurma <maheshruby405@gmail.com>
2020-06-09 18:31:06 -0400

    refs #33 exception handling- RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# By default, only the ApplicationController in a Rails application
+# inherits from Jets::Controller::Base. All other controllers inherit
+# from ApplicationController. This gives you one class to configure
+# things such as request forgery protection and filtering of sensitive
+# request parameters.
 class ApplicationController < Jets::Controller::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :missing_record
+
+  def missing_record
+    render json: { message: "Couldn't find Record" }, status: 404
+  end
 end
