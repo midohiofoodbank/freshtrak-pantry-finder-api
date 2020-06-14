b190bf081117e8e8d38d78ae536f49c68b2fc9fa
Mahesh Kurma <maheshruby405@gmail.com>
2020-06-13 20:17:16 -0400

    code refactor

--- app/serializers/event_hour_serializer.rb
+++ app/serializers/event_hour_serializer.rb
@@ -4,13 +4,5 @@
 class EventHourSerializer < ApplicationSerializer
   attributes :event_hour_id, :start_time, :end_time, :open_slots
 
-  def open_slots
-    object&.open_slots
-  end
-
-  has_many :event_slots, if: -> { should_render_association }
-
-  def should_render_association
-    @instance_options.key?(:event_slots) ? true : false
-  end
+  has_many :event_slots, if: -> { @instance_options[:event_slots] }
 end
