50394c7960695e97ceace9fcd11f188c331188bf
PhilNorman2 <31697968+PhilNorman2@users.noreply.github.com>
2020-07-14 11:28:04 -0400

    Dynamic age controls in events display (#41)
    * Dynamic age controls in events display
    
    * removed extraneous comments from spec files
    
    * simplified within_range scope test in form_spec.rb
    
    * additional tests for form_spec.rb and misc. changes
    
    * added two more tests in form_spec.rb
    
    * linter fixes
    
    * moved form data to form model and serializer, added scope to limit to language_id == 1
    
    * moved master_form_num out of event factory
    
    * minor cleanup

---
+++ app/models/form.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Form, associated with an event
+class Form < ApplicationRecord
+  alias_attribute :id, :form_id
+
+  belongs_to :event, primary_key: :form_master_num,
+                     foreign_key: :form_master_num, inverse_of: :forms
+
+  default_scope { active.language.within_range }
+  scope :active, -> { where(status_id: 1) }
+  # limit to language_id == 1 (english)
+  scope :language, -> { where(language_id: 1) }
+  scope :within_range, lambda {
+    where('? between effective_start and effective_end', Date.today)
+  }
+
+  def display_age_adult
+    max_age_child + 1
+  end
+
+  def display_age_senior
+    max_age_adult + 1
+  end
+end
