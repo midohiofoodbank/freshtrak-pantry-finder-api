5b9319762836e5e3a21e19284b0695f74894408c
Phil Norman <philnorman2@gmail.com>
2020-08-20 13:54:02 -0400

    added active scope and test for foodbank_text model

--- app/models/foodbank_text.rb
+++ app/models/foodbank_text.rb
@@ -3,5 +3,9 @@
 # Foodbank, associated to many Foodbank Texts
 class FoodbankText < ApplicationRecord
   self.table_name = 'fb_text'
+
   belongs_to :foodbank, foreign_key: :fb_id, inverse_of: :foodbank_texts
+
+  default_scope { active }
+  scope :active, -> { where(status_id: 1) }
 end
