8421c8b0e2c5d28f2fc9931c5134c106557f86a2
sarath-yerra <sarathpy.02@gmail.com>
2020-09-09 20:16:53 -0400

    adding event details method to handle external url in fresh trak

--- app/controllers/api/event_dates_controller.rb
+++ app/controllers/api/event_dates_controller.rb
@@ -3,13 +3,20 @@
 module Api
   # Controller to expose Event Dates
   class EventDatesController < Api::BaseController
-    before_action :set_event_date, only: [:show]
+    before_action :set_event_date, only: %i[show event_details]
 
     # GET /api/event_dates/:id
     def show
       render json:
         ActiveModelSerializers::SerializableResource.new(
           @event_date, event_hours: true, event_slots: true
+        ).as_json
+    end
+
+    def event_details
+      render json:
+        ActiveModelSerializers::SerializableResource.new(
+          @event_date.event
         ).as_json
     end
 
--- spec/controllers/api/event_dates_controller_spec.rb
+++ spec/controllers/api/event_dates_controller_spec.rb
@@ -20,4 +20,12 @@ describe Api::EventDatesController, type: :controller do
     expect(event_date_response['error']).to eq('record_not_found')
     expect(event_date_response['message']).not_to be(nil)
   end
+
+  it 'shows the event details' do
+    get "/api/event_dates/#{event_date.id}/event_details"
+
+    expect(response.status).to eq 200
+    event_date_response = JSON.parse(response.body)['event']['event_dates'][0]
+    expect(event_date_response['id']).to eq(event_date.id)
+  end
 end
