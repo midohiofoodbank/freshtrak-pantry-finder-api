01ac805a3d620b6e3262acb663e48b4344369077
Phil Norman <philnorman2@gmail.com>
2020-11-03 16:23:12 -0500

    added controller and model tests

--- spec/controllers/api/events_controller_spec.rb
+++ spec/controllers/api/events_controller_spec.rb
@@ -18,4 +18,12 @@ describe Api::EventsController, type: :controller do
     expect(event_response['error']).to eq('record_not_found')
     expect(event_response['message']).not_to be(nil)
   end
+
+  it 'is indexable by event_date_id' do
+    event_date = create(:event_date, event: event)
+    get '/api/events', event_date_id: event_date.id
+    expect(response.status).to eq 200
+    response_body = JSON.parse(response.body)
+    expect(response_body['events'].count).to eq(1)
+  end
 end
