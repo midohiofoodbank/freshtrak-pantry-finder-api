636840f4b85dea6e101c15c5fef6bc4a3ccd2564
Phil Norman <philnorman2@gmail.com>
2020-11-02 19:20:43 -0500

    event_date_id param and scope

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,7 +3,16 @@
 module Api
   # Controller to expose Events
   class EventsController < Api::BaseController
+    before_action :set_events, only: [:index]
     before_action :set_event, only: [:show]
+
+    def index
+      if (@event_date_id = search_params[:event_date_id])
+        @events = @events.with_event_date_id(@event_date_id)
+      end
+
+      render json: serialized_events
+    end
 
     # GET /api/events/:id
     def show
@@ -13,8 +22,25 @@ module Api
 
     private
 
+    def search_params
+      params.permit(:event_date_id)
+    end
+
+    def set_events
+      @events =
+        if !search_params[:event_date_id]
+          Event.none
+        else
+          Event.distinct
+        end
+    end
+
     def set_event
       @event = Event.find(params[:id])
+    end
+
+    def serialized_events
+      ActiveModelSerializers::SerializableResource.new(@events).as_json
     end
   end
 end
