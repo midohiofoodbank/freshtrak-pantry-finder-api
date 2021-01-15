8847d66a8925253d4f5378e242a76c61cd168fba
maheshkurma <maheshruby405@gmail.com>
2021-01-15 16:36:28 -0500

    update api to filter events by distance

--- app/controllers/api/agencies_controller.rb
+++ app/controllers/api/agencies_controller.rb
@@ -3,17 +3,17 @@
 module Api
   # Exposes the Agency location data
   class AgenciesController < Api::BaseController
+    before_action :permit_params
     before_action :set_agencies, only: [:index]
     before_action :set_user_location, only: [:index]
     before_action :set_agency, only: [:show]
 
     def index
-      if (@zip = search_params[:zip_code])
-        @agencies = @agencies.by_zip_code(@zip)
-      end
-      if (date = search_params[:event_date])
-        @agencies = @agencies.with_event_after(date.delete('-'))
-      end
+      by_zip(@zip)
+
+      by_zip_and_distance(@zip, @distance)
+
+      with_event_after(@event_date)
 
       render json: serialized_agencies
     end
@@ -26,13 +26,21 @@ module Api
 
     private
 
-    def search_params
-      params.permit(:zip_code, :event_date, :lat, :long)
+    def permit_params
+      params.permit(
+        :zip_code, :distance, :event_date, :event_date_on, :lat, :long
+      ).tap do |param|
+        @zip = param[:zip_code]
+        @distance = param[:distance]
+        @event_date = param[:event_date]
+        @lat = param[:lat]
+        @long = param[:long]
+      end
     end
 
     def set_agencies
       @agencies =
-        if !search_params[:zip_code] && !search_params[:event_date]
+        if !@zip && !@distance && !@event_date
           Agency.none
         else
           Agency.distinct
@@ -50,11 +58,9 @@ module Api
     # :zip_code paramter (used to calculate distance from the zip code
     # (centroid)
     def set_user_location
-      return unless (search_params[:lat] && search_params[:long]) ||
-                    search_params[:zip_code]
+      return unless (@lat && @long) || @zip
 
-      user_location_object(search_params[:lat], search_params[:long],
-                           search_params[:zip_code])
+      user_location_object(@lat, @long, @zip)
     end
 
     def user_location_object(lat, long, zip_code)
@@ -71,6 +77,24 @@ module Api
                                                        @user_location,
                                                        zip_code: @zip)
                                                   .as_json
+    end
+
+    def by_zip(zip)
+      return unless zip
+
+      @agencies = @agencies.by_zip_code(zip)
+    end
+
+    def by_zip_and_distance(zip, distance)
+      return unless zip && distance
+
+      @agencies = @agencies.select { |ag| ag.estimated_distance(@user_location) <= distance.to_f }
+    end
+
+    def with_event_after(date)
+      return unless date
+
+      @agencies = @agencies.with_event_after(date.delete('-'))
     end
   end
 end
